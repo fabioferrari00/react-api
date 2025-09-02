154efbc1a8bac3fbed4477c75cc2f14fc203ce25
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-02 15:23:19 +0200

    [feat] milestone 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,49 @@ import axios from "axios";
 
 function App() {
 
-  axios
-    .get(`https://lanciweb.github.io/demo/api/actors/`)
-    .then((resp) => {
-      console.log(resp.data)
-    })
-  axios
-    .get(`https://lanciweb.github.io/demo/api/actresses/`)
-    .then((resp) => {
-      console.log(resp.data)
-    })
+  const [actors, setActors] = useState([]);
+
+  const fetchActor = () => {
+    axios
+      .get(`https://lanciweb.github.io/demo/api/actors/`)
+      .then((resp) => {
+        console.log(resp.data)
+        setActors(resp.data)
+      })
+    axios
+      .get(`https://lanciweb.github.io/demo/api/actresses/`)
+      .then((resp) => {
+        console.log(resp.data)
+      })
+  }
+
+  useEffect(fetchActor, []);
 
   return (
     <>
       <div className="container">
-        <div className="row">
+        <div className="row gy-3">
           <div className="col-12">
             <h1 className="text-center">Cast:</h1>
           </div>
-          <div className="col-12 col-md-4">
-            <div class="card">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/220px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png" className="card-img-top" alt="..." />
-              <div class="card-body">
-                <h5 class="card-title">Nome:</h5>
-                <span class="card-text">Anno di nascita:</span>
-                <span class="card-text">Nazionalità:</span>
-                <p class="card-text"><small class="text-body-secondary">Biografia:</small></p>
-                <p class="card-text"><small class="text-body-secondary">Known for:</small></p>
+          {actors.map((actor) => {
+            return (
+              <div className="col-12 col-md-4" key={actor.id}>
+                <div className="card" >
+                  <img src={actor.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">Nome: {actor.name}</h5>
+                    <span className="card-text">Anno di nascita: {actor.birth_year}</span>
+                    <p className="card-text">Nazionalità: {actor.nationality}</p>
+                    <p className="card-text"><small className="text-body-secondary">Biografia: {actor.biography}</small></p>
+                    <p className="card-text"><small className="text-body-secondary">Known for: {actor.known_for.join(", ")}</small></p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
+            )
+          })
+
+          }
         </div>
       </div>
     </>
