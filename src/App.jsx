9add8b4443283f824b80611020aadc920d6e6bd5
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-02 16:09:12 +0200

    [fix] style

--- src/App.jsx
+++ src/App.jsx
@@ -23,33 +23,34 @@ function App() {
 
   return (
     <>
-      <div className="bg-lg-gray">
-        <div className="container ">
-          <div className="row gy-3">
-            <div className="col-12">
-              <h1 className="text-center">Cast:</h1>
-            </div>
-            {actors.map((actor) => {
-              return (
-                <div className="col-12 col-md-4" key={actor.id}>
-                  <div className="card" >
-                    <img src={actor.image} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                      <div className="head-card mb-3">
-                        <h5 className="card-title mt-2">Nome: {actor.name}</h5>
-                        <span className="card-text">Anno di nascita: <strong>{actor.birth_year}</strong></span>
-                        <p className="card-text">Nazionalità: <strong>{actor.nationality}</strong></p>
-                        <p className="card-text mb-3"><small className="text-body-secondary">Biografia: {actor.biography}</small></p>
-                      </div>
+      <header className="bg-lg-gray">
+        <h1 className="py-3">The best cast</h1>
+      </header>
+      <div className="container ">
+        <div className="row gy-4">
+          <div className="col-12">
+            <h1 className="text-center">Actors:</h1>
+          </div>
+          {actors.map((actor) => {
+            return (
+              <div className="col-12 col-md-4" key={actor.id}>
+                <div className="card bg-lg-gray" >
+                  <img src={actor.image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <div className="mb-3">
+                      <h5 className="card-title mt-2">Nome: {actor.name}</h5>
+                      <span className="card-text">Anno di nascita: <strong>{actor.birth_year}</strong></span>
+                      <p className="card-text">Nazionalità: <strong>{actor.nationality}</strong></p>
+                      <p className="card-text mb-3"><small className="text-body-secondary">Biografia: {actor.biography}</small></p>
                       <p className="card-text"><strong>Known for: {actor.known_for.join(", ")}</strong></p>
                     </div>
                   </div>
                 </div>
-              )
-            })
+              </div>
+            )
+          })
 
-            }
-          </div>
+          }
         </div>
       </div>
     </>
