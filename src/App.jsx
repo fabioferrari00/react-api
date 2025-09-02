8942423ca09a8f65bdb3ea63e717bbf0fc17005d
fabioferrari00 <fabioferrari.ff00@gmail.com>
2025-09-02 15:06:09 +0200

    [feat] milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,16 @@ function App() {
         <div className="row">
           <div className="col-12">
             <h1 className="text-center">Cast:</h1>
+          </div>
+          <div className="col-12 col-md-4">
             <div class="card">
-              <img src="..." className="card-img-top" alt="..." />
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Marlon_Brando_publicity_for_One-Eyed_Jacks.png/220px-Marlon_Brando_publicity_for_One-Eyed_Jacks.png" className="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Nome</h5>
-                <span class="card-text">Anno di nascita</span>
-                <span class="card-text">Nazionalità</span>
-                <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                <h5 class="card-title">Nome:</h5>
+                <span class="card-text">Anno di nascita:</span>
+                <span class="card-text">Nazionalità:</span>
+                <p class="card-text"><small class="text-body-secondary">Biografia:</small></p>
+                <p class="card-text"><small class="text-body-secondary">Known for:</small></p>
               </div>
             </div>
           </div>
